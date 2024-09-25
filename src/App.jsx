b0925de307f5a256eceaede35b7619f6395cf089
DanielMjosRoli <daniel.mrfush@gmail.com>
2024-09-25 15:12:38 +0200

    Done with all extensions and core :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import emailData from './data/emails'
 import initialEmails from './data/emails'
 
 import './styles/App.css'
@@ -6,6 +8,43 @@ import './styles/App.css'
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+  const [emails, setEmail] = useState(emailData)
+  const [toggled, setToggled] = useState('Inbox')
+  const [removeRead, setRemoveRead] = useState(false)
+
+  const getEmails = () => {
+    if(toggled === 'Inbox'){
+      if(removeRead){
+        let emailList = emails.filter( (e) => !e.read)
+        return emailList
+      }
+      return emails
+    }else if (toggled === 'Starred'){
+      if(removeRead){
+        let emailList = emails.filter( (e) => e.starred && !e.read)
+        return emailList
+      }
+      let emailList = emails.filter( (e) => e.starred)
+      return emailList
+    }
+    return []
+    
+  }
+  const countStarred = () => {
+    if(removeRead){
+      let emailList = emails.filter( (e) => e.starred && !e.read)
+      return emailList.length
+    }
+    let emailList = emails.filter( (e) => e.starred)
+    return emailList.length
+  }
+  const countInbox = () => {
+    if(removeRead){
+      let emailList = emails.filter( (e) => !e.read)
+      return emailList.length
+    }
+    return emails.length
+  }
 
   return (
     <div className="app">
@@ -13,18 +52,18 @@ function App() {
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
-            className="item active"
-            // onClick={() => {}}
+            className={toggled === 'Inbox'? "item active" : "item"}
+            onClick={() => {setToggled('Inbox')}}
           >
             <span className="label">Inbox</span>
-            <span className="count">?</span>
+            <span className="count">{countInbox()}</span>
           </li>
           <li
-            className="item"
-            // onClick={() => {}}
+            className={toggled === 'Starred'? "item active" : "item"}
+            onClick={() => {setToggled('Starred')}}
           >
             <span className="label">Starred</span>
-            <span className="count">?</span>
+            <span className="count">{countStarred()}</span>
           </li>
 
           <li className="item toggle">
@@ -32,13 +71,46 @@ function App() {
             <input
               id="hide-read"
               type="checkbox"
-              checked={false}
-              // onChange={() => {}}
+              checked={removeRead}
+              onChange={() => {setRemoveRead(!removeRead)}}
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{getEmails().map(
+        (object) => 
+          <li className={object.read ? "email read" : "email unread"} key = {object.id}>
+            <div className="select">
+              <input
+                className="select-checkbox"
+                type="checkbox"
+                checked = {object.read}
+                onChange={ () => {
+                  object.read = !object.read
+                  setEmail([...emails])
+                }}
+                />
+                
+            </div>
+            <div className="star">
+              <input
+                className="star-checkbox"
+                type="checkbox"
+                checked = {object.starred}
+                onChange={() => {
+                  object.starred = !object.starred
+                  setEmail([...emails])
+                }}
+              />
+            </div>
+            <div className="sender">
+              {object.sender}
+            </div>
+            <div className="title">
+              {object.title}
+            </div>
+          </li>
+      )}</main>
     </div>
   )
 }
